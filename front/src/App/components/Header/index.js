52a2d045c1395408e1028f04a84f6bcacf2718ff
livelikeabel <esung1129@gmail.com>
2018-09-02 02:36:33 +0900

    add header component css

--- front/src/App/components/Header/index.js
+++ front/src/App/components/Header/index.js
@@ -3,15 +3,33 @@ import React, { Component } from 'react'
 import classnames from 'classnames/bind'
 import css from './index.scss'
 
+import menuIcon from '../../../assets/icons/menu.png'
+import logo from '../../../assets/logos/logo.png'
+
 const cx = classnames.bind(css)
 const moduleName = 'Header'
 
 class Header extends Component {
   render() {
-    return <div className={cx(`${moduleName}`)}>
-      <header>Semi<br/>Basement</header>
-    </div>
+    return (
+      <header className={cx(`${moduleName}`)}>
+        <div className={cx(`${moduleName}-menu`)}>
+          <img src={menuIcon} alt="menuIcon" />
+        </div>
+        <div className={cx(`${moduleName}-logo`)}>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className={cx(`${moduleName}-rightSide`)}>
+          <div className={cx(`${moduleName}-rightSide-user`)}>
+            <span className={cx(`${moduleName}-rightSide-user-image`)}></span>
+            <span>userName</span>
+            <span>></span>
+          </div>
+          <div className={cx(`${moduleName}-rightSide-playlist`)}>playlist</div>
+        </div>
+      </header>
+    )
   }
-} 
+}
 
-export default Header;
+export default Header
