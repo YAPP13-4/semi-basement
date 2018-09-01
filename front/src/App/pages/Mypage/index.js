02c5804bec47d42ce3234902a8df3fc6f1f3076b
livelikeabel <esung1129@gmail.com>
2018-09-01 22:44:26 +0900

    scss 파일 연결

--- front/src/App/pages/Mypage/index.js
+++ front/src/App/pages/Mypage/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 
+import classnames from 'classnames/bind'
+import css from './index.scss'
+
+const cx = classnames.bind(css)
+const moduleName = 'Mypage'
+
 class Mypage extends Component {
   render() {
-    return <div>Mypage이야</div>
+    return <div className={cx(`${moduleName}`)}>Mypage이야</div>
   }
 } 
 
--- front/src/App/pages/Sign/index.js
+++ front/src/App/pages/Sign/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 
+import classnames from 'classnames/bind'
+import css from './index.scss'
+
+const cx = classnames.bind(css)
+const moduleName = 'Sign'
+
 class Sign extends Component {
   render() {
-    return <div>Sign이야</div>
+    return <div className={cx(`${moduleName}`)}>Sign이야</div>
   }
 } 
 
