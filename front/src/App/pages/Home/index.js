e18b299783c5dadca7d19b1521eab82ee857b7fe
SoYoung210 <ethdud1@gamil.com>
2018-09-02 00:00:33 +0900

    img flex 배치후 scss 파일 수정

--- front/src/App/pages/Home/index.js
+++ front/src/App/pages/Home/index.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react'
 
 
 // mp3 files
-import Billy from 'src/assets/mp3/Billy.mp3';
-import Bryson from 'src/assets/mp3/Bryson Tiller - Canceled (prod By Hunga).mp3';
-import No_Rest from 'src/assets/mp3/No Rest (Prod. by @Menohbeats).mp3';
-import sheck_wes from 'src/assets/mp3/sheck wes - mo bamba (prod. 16yrold & take a daytrip).mp3'
-import Walk from 'src/assets/mp3/Taking A Walk (Prod. Scott Storch).mp3'
-import BillyThumbnail from 'src/assets/thumbnail/Billy.jpg'
+import Billy from 'src/assets/thumbnail/Billy.jpg';
+import Bryson from 'src/assets/thumbnail/Bryson Tiller - Canceled (prod By Hunga).jpg';
+import No_Rest from 'src/assets/thumbnail/No Rest (Prod. by @Menohbeats).jpg';
+import sheck_wes from 'src/assets/thumbnail/sheck wes - mo bamba (prod. 16yrold & take a daytrip).jpg'
+import Walk from 'src/assets/thumbnail/Taking A Walk (Prod. Scott Storch).jpg'
 
 import Song from './Song'
 
@@ -21,22 +20,22 @@ const moduleName = 'Home'
 class Home extends Component {
   state = {}
   componentDidMount() {
-    this._getSong();
+    this._getSongThumbnail();
   }
-  _getSong = () => {
-    const songs = [Billy, Bryson, No_Rest, sheck_wes, Walk]
-    console.log(songs);
+  _getSongThumbnail = () => {
+    const songsThumbnail = [Billy, Bryson, No_Rest, sheck_wes, Walk]
+
     this.setState({
-      songs
+      songsThumbnail
     })
   }
   _renderSong = () => {
-    const songs = this.state.songs.map((mp3, index) => {
-      console.log(mp3)
-      return <Song thumbnail={BillyThumbnail}
-        singer="soyoung"
-        title="Billy"
-        mp3={mp3}
+    const songs = this.state.songsThumbnail.map((img, index) => {
+      console.log(img)
+      return <Song thumbnail={img}
+                  singer="soyoung"
+                  title="Billy"
+                  mp3={"mp3"}
       />
     })
     return songs
@@ -44,8 +43,8 @@ class Home extends Component {
   render() {
     return (
       <div className={cx(`${moduleName}`)}>
-        <div className="Song__Wrapper">
-          {this.state.songs ? this._renderSong() : 'Loading'}
+        <div className={cx(`${moduleName}-songWrapper`)}>
+          {this.state.songsThumbnail ? this._renderSong() : 'Loading'}
         </div>
       </div>
 
