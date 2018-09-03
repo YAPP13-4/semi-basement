d2f1cda571d75939bc01b1d07cbe27536a01b373
SoYoung210 <ethdud1@gamil.com>
2018-09-03 10:35:50 +0900

    #5 bottom player 하단 고정 (크기는 임시 1440px) 나머지 대응은 나중에!

--- front/src/App/components/BottomPlayer/index.js
+++ front/src/App/components/BottomPlayer/index.js
@@ -7,19 +7,14 @@ import first from './Billy.mp3'
 const cx = classnames.bind(css)
 const moduleName = 'BottomPlayer'
 
-
-
-
-
-
-
 // const cx = classnames.bind(css)
 
 class BottomPlayer extends Component {
     render() {
         return (
           <div className={cx(`${moduleName}`)}>
-            <ReactPlayer className={cx(`${moduleName}-player`)} url={first} controls={true}/>
+            
+            <ReactPlayer className={cx(`${moduleName}-player`) } url={first} controls={true}/>
           </div>
         )
       }
