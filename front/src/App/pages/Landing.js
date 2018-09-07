02cacf5d2500b72a10752e166ba6beb93adb99d0
SoYoung210 <ethdud1@gmail.com>
2018-09-08 00:35:44 +0900

    #12 landing page 클래스 컴포넌트로 변경 및 mapDispatchToProps 적용

--- front/src/App/pages/Landing.js
+++ front/src/App/pages/Landing.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React ,{Component} from 'react'
 import classnames from 'classnames/bind'
 import css from './Landing.scss'
 import landingtitle from '../../assets/landing-title.png'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { landing } from '../../actions/index'
 const cx = classnames.bind(css)
 const moduleName = 'Landing'
 
+class Landing extends Component {
+    constructor(props) {
+        super(props)
 
-const Landing = () => {
-    return (
-        <div className={cx(`${moduleName}`)}>
-           <div className={cx(`${moduleName}-logoWrapper`)}>
-            <img src={landingtitle} alt="landing title"/>
-            <a>
-                <span>GET INTO</span>
-            </a>
-           </div>
-        </div>
-    )
+        this.state = { term : 1}
+        this._getInto = this._getInto.bind(this)
+    }
+    _getInto(event) {
+        this.setState({
+            term : 1
+        });
+        this.props.landing(this.state.term)
+    }
+    render() {
+        return (
+            <div className={cx(`${moduleName}`)}>
+                <div className={cx(`${moduleName}-logoWrapper`)}>
+                    <img src={landingtitle} alt="landing title"/>
+                    <a>
+                        <span>GET INTO</span>
+                    </a>
+                </div>
+            </div>
+        )
+    }
+}
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({landing} , dispatch)
 }
 
-export default Landing;
+export default connect(null, mapDispatchToProps)(Landing)
