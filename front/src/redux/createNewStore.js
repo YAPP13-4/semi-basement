1d71127a6dc88449949ed6870a5287129fbdb6af
livelikeabel <esung1129@gmail.com>
2018-09-09 22:27:22 +0900

    #12 add createNewStore

---
+++ front/src/redux/createNewStore.js
@@ -0,0 +1,31 @@
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+
+import reducer from 'src/redux/reducers';
+
+
+export default function createNewStore(history) {
+  const routerMw = routerMiddleware(history);
+
+  const store = createStore(
+    combineReducers({
+      router: routerReducer,
+      ...reducer,
+    }),
+    {},
+    compose(
+      applyMiddleware(routerMw),
+      typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
+    )
+  );
+
+  if (module.hot) {
+    module.hot.accept(() => {
+      store.replaceReducer(require('src/redux/reducers').default);
+    });
+  }
+
+  return store;
+}
