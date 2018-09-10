394deae6521d0adea8da5d8e6fea26f8b04ca915
SoYoung210 <ethdud1@gmail.com>
2018-09-10 23:09:38 +0900

    #20 artwork 쪽 div 구조 잡기

--- front/src/App/pages/Home/components/ArtworkPlay.js
+++ front/src/App/pages/Home/components/ArtworkPlay.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react';
-import { callApi } from '../../../../utils/ApiUtils'
-import axios from 'axios';
-import { resolveUrl,SONG_URL } from '../../../constants/ApiConstants'
-class ArtworkPlay extends Component {
-    state = {}
-    constructor() {
-        super();
-    }
-    componentDidMount() {
-        this._requestId();
-      }
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind'
+import css from './ArtworkPlay.scss'
 
-    _requestId = () => {
-        return axios.get(resolveUrl('https://soundcloud.com/matas/frost-theme-0-1'))
-        .then(response => {
-            this.setState({
-                singerId : response.data.id
-            })    
-        })
-        .catch(err => console.log(err))
-    }
+const cx = classnames.bind(css)
+const moduleName = 'ArtworkPlay'
+
+
+class ArtworkPlay extends Component {
     render() {
         return(
-            <div style={{color:'#ffffff'}}>
-                {this.state.singerId}
-            </div>
+            <Fragment>
+                <div >
+                    SEBA's Choice
+                </div>
+
+                <div className="row">
+                    <div className="row__cell">
+                        <div className="song-body-card">
+                            <div className ="song-body-card-inner">
+                                <div 
+                                    className="song-body-card-artwork"
+                                    style= {{backgroundImage: this.props.artwork}}
+                                >
+
+                                </div>
+                                <div className ="song-body-card-title">
+                                </div>
+                                <div className ="song-body-card-singer">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
         )
     }
 }
+
 export default ArtworkPlay;
