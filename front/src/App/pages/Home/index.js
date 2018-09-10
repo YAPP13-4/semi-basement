fe80f523f75c3796bc3707c041cc3f08ac8ca545
SoYoung210 <ethdud1@gmail.com>
2018-09-10 23:09:13 +0900

    #20 index.js에서 Artwork 쪽으로 props 전달

--- front/src/App/pages/Home/index.js
+++ front/src/App/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-
+import { callApi } from '../../../utils/ApiUtils'
+import axios from 'axios';
+import { resolveUrl,SONG_URL } from '../../constants/ApiConstants'
 
 import ArtworkPlay from './components/ArtworkPlay'
 
@@ -13,13 +15,36 @@ const moduleName = 'Home'
 
 class Home extends Component {
   state = {}
+  constructor() {
+      super();
+  }
+  componentDidMount() {
+      this._requestId();
+    }
+
+  _requestId = () => {
+      return axios.get(resolveUrl('https://soundcloud.com/matas/frost-theme-0-1'))
+      .then(response => {
+        console.log(response.data)
+          console.log(response.data.id)
+          console.log(response.data.artwork_url)
+          this.setState({
+              singerId : response.data.id,
+              artwrokUrl : response.data.artwork_url
+          })    
+      })
+      .catch(err => console.log(err))
+  }
 
   render() {
     return (
       <div className={cx(`${moduleName}`)}>
         <Navigation />
         <div className={cx(`${moduleName}-songWrapper`)}>
-          <ArtworkPlay />
+          <ArtworkPlay 
+            id = {this.state.singerId}
+            artwork = {this.state.artwrokUrl}
+          />
         </div>
       </div>
     )
