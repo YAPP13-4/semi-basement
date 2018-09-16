245d503665d15a546320bc6e22ca712bbcc9e699
SoYoung210 <ethdud1@gmail.com>
2018-09-16 15:41:03 +0900

    #23 데이터 셋팅 , 레이아웃잡기 1

--- front/src/App/pages/Home/Chart/ChartTab.js
+++ front/src/App/pages/Home/Chart/ChartTab.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { selectSong } from '../../../../actions/index'
+import axios from 'axios';
+import { resolveUrl } from '../../../constants/ApiConstants'
 import classnames from 'classnames/bind'
+import SongChartList from './constants/test/SongChartList' 
 
 import css from './ChartTab.scss'
 import ChartTabItem from './containers/ChartTabContainer'
-const cx = classnames.bind(css)
+const cx         = classnames.bind(css)
 const moduleName = 'ChartTab'
-//ChartTab에서 state 관리해서 던지기 .. ? 
+
 class ChartTab extends Component {
-    state = {}
+    state = {
+        songInfos: [],
+      }
+    componentDidMount() {
+        this._requestId();
+      }
+
+    _requestId = () => {
+        SongChartList.map( (url)=> {
+        return axios.get(resolveUrl(url))
+              .then(response => {
+                this.setState({
+                  songInfos: this.state.songInfos.concat(response.data)
+                }) 
+                //return response.data;
+              })
+      })
+    }
+    _fetchSong = (songInfo) => {
+      console.log('click',songInfo);
+      this.props.selectSong(songInfo.id) // 속성 뭔지 확인해서 고치기 
+    }
+    _renderChart = () => {
+        const songs = this.state.songInfos.map( (songInfo,index)=> {
+            return <ChartTabItem 
+                        key      = {index}
+                        singer   = {songInfo.user.permalink}
+                        title    = {songInfo.title}
+                        artwork  = {songInfo.artwork_url}
+                        selected = {()=>{this._fetchSong(songInfo)}}
+                    />
+        })
+        return songs;
+    }
     //나중에 서버에서 song을 받아오면... .. state 수정해서 넣어야지 ..
     render() {
         return(
@@ -20,11 +58,13 @@ class ChartTab extends Component {
                     </select>
                 </div>
                 <div className={cx(`${moduleName}-chart`)}>
-                    <ChartTabItem/>
+                    {this.state.songInfos ? this._renderChart() : 'Loading'}  
                 </div>
             </div>
         )
     }
 }
-
-export default ChartTab;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({selectSong} , dispatch)
+}
+export default connect(null,mapDispatchToProps)(ChartTab);
--- front/src/App/pages/Home/Chart/containers/ChartTabContainer.js
+++ front/src/App/pages/Home/Chart/containers/ChartTabContainer.js
@@ -1,47 +1,28 @@
 import React ,{Component} from 'react'
 import classnames from 'classnames/bind'
 import css from './ChartTabContainer.scss';
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { selectSong } from '../../../../../actions/index'
-const cx = classnames.bind(css)
+import getImageUrl from '../../../../../utils/ImageUtils'
+import IMAGE_SIZES from '../../../../constants/ImageConstants'
+
+const cx         = classnames.bind(css)
 const moduleName = 'ChartTabContainer'
 
 class chartItem extends Component{
-    constructor(props) {
-        super(props)
 
-        this.state = { term : ''}
-        this._onSongSelected = this._onSongSelected.bind(this);
-    }
-    _onSongSelected(event) {
-        console.log(event.target.id);
-        this.setState({
-            term : event.target.id
-        });
-        this.props.selectSong(this.state.term)
-    }
     render() {
+
         return (
             <div className={cx(`${moduleName}`)}>
                 <table>
                     <tr>
-                        <td onClick={this._onSongSelected} id="https://soundcloud.com/uiceheidd/lucid-dreams-forget-me">Lucid Dreams</td>
-                        <td>Creator</td>
-                        <td>Creator</td>
-                        <td>icons</td>
-                    </tr>
-
-                     <tr>
-                        <td onClick={this._onSongSelected} id="https://soundcloud.com/trippie-hippie-2/taking-a-walk-prod-scott-storch">Taking A walk</td>
-                        <td>Creator</td>
-                        <td>Creator</td>
-                        <td>icons</td>
-                    </tr>
-                    <tr>
-                        <td onClick={this._onSongSelected} id="https://soundcloud.com/kinggoldchains/taste-feat-offset">Taste</td>
-                        <td>Creator</td>
-                        <td>Creator</td>
+                        <td >{this.props.key} </td>
+                        <td onClick={this.props.selected}>
+                            <img  src={`url(${getImageUrl(this.props.artwork, IMAGE_SIZES.LARGE)})`}/>
+                        </td>
+                        <td onClick={this.props.selected}>Lucid Dreams</td>
+                        <td>{this.props.title}</td>
+                        <td>{this.props.singer}</td>
+                        <td>02: 27(time)</td>
                         <td>icons</td>
                     </tr>
                 </table>
@@ -50,7 +31,5 @@ class chartItem extends Component{
         )
     }
 }
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({selectSong} , dispatch)
-}
-export default connect(null, mapDispatchToProps)(chartItem)
+
+export default chartItem
