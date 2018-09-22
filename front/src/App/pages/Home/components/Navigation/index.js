ce71872d9c737f4501d2faf842540781c2289be7
livelikeabel <esung1129@gmail.com>
2018-09-22 19:42:49 +0900

    path 에 따라 navbar의 밑줄 활성화 완료

--- front/src/App/pages/Home/components/Navigation/index.js
+++ front/src/App/pages/Home/components/Navigation/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
 import { Link } from 'react-router-dom';
 import classnames from 'classnames/bind'
 import css from './index.scss'
@@ -7,12 +9,21 @@ const cx = classnames.bind(css)
 const moduleName = 'Navigation'
 
 class Navigation extends Component {
+
   render() {
+
+    const paintingCurrentStyle = (path) => {
+      if(this.props.pathname === `${path}`) return underStyle
+    }
+
+    const underStyle = {
+      borderBottom: '2px solid #45f7aa'
+    }
     return (
       <div className={cx(`${moduleName}`)}>
         <div className={cx(`${moduleName}-rightBox`)}>
-          <div><Link to ="/main">Discover</Link></div>
-          <div><Link to ="/chart">Chart</Link></div>
+          <div style={paintingCurrentStyle('/main')}><Link to ="/main">Discover</Link></div>
+          <div style={paintingCurrentStyle('/chart')}><Link to ="/chart">Chart</Link></div>
           <div><Link to ="/recent">Recent</Link></div>
         </div>
         <div className={cx(`${moduleName}-leftBox`)}>
@@ -26,4 +37,13 @@ class Navigation extends Component {
   }
 }
 
-export default Navigation
+export default compose(
+  connect(state => {
+    const { router } = state
+    return {
+      pathname: router.location.pathname
+    }
+  }
+
+  )
+)(Navigation)
