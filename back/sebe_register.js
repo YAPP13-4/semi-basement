26e13c92ccdf0729493e675bc2d395330b625fc9
SkynI25 <rlxo1651@daum.net>
2018-09-23 01:40:47 +0900

    #32 뮤직리스트 구현 ; axios를 통해 url로부터 값을 추출하고 db에 저장

--- back/sebe_register.js
+++ back/sebe_register.js
@@ -9,11 +9,13 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const mysql = require('mysql');
+const axios = require('axios');
+
 const conn = mysql.createConnection({
-    host : db_config.host,
-    user : db_config.user,
-    password : db_config.password,
-    database : db_config.database
+    host: db_config.host,
+    user: db_config.user,
+    password: db_config.password,
+    database: db_config.database
 })
 conn.connect();
 const app = express();
@@ -32,8 +34,8 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.get('/welcome', function(req, res) {
-    if(req.user && req.user.displayName) {
+app.get('/welcome', function (req, res) {
+    if (req.user && req.user.displayName) {
         res.send(`
         <h1>Hello, ${req.user.displayName}</h1>
         <a href="/auth/logout">Logout</a>
@@ -48,15 +50,15 @@ app.get('/welcome', function(req, res) {
         `);
     }
 });
-app.get('/auth/logout', function(req, res){
+app.get('/auth/logout', function (req, res) {
     req.logout();
-    req.session.save(function() {
+    req.session.save(function () {
         res.redirect('/welcome');
     });
 });
-app.get('/auth/register', function(req, res) {
+app.get('/auth/register', function (req, res) {
     let output =
-    `
+        `
     <h1>Register</h1>
     <form action="/auth/register" method="post">
      <p>
@@ -76,7 +78,7 @@ app.get('/auth/register', function(req, res) {
     res.send(output);
 });
 
-app.get('/auth/login', function(req, res){ 
+app.get('/auth/login', function (req, res) {
     let output = `
     <h1>Login</h1>
     <form action="/auth/login" method="post">
@@ -95,16 +97,16 @@ app.get('/auth/login', function(req, res){
     `;
     res.send(output);
 });
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     //console.log('serializeUser', user);
     done(null, user.authId);
 });
-  
-passport.deserializeUser(function(id, done) {
+
+passport.deserializeUser(function (id, done) {
     //console.log('deserializeUser', id);
     let sql = 'SELECT * FROM users WHERE authId=?';
-    conn.query(sql, [id], function(err, results) {
-        if(err) {
+    conn.query(sql, [id], function (err, results) {
+        if (err) {
             console.log(err);
             done('There is no user.');
         } else {
@@ -113,12 +115,12 @@ passport.deserializeUser(function(id, done) {
     });
 });
 passport.use(new LocalStrategy(
-    function(username, password, done) {
+    function (username, password, done) {
         let uname = username;
         let pwd = password;
         let sql = 'SELECT * FROM users WHERE authId=?';
-        conn.query(sql, ['google:'+uname], function(err, results) {
-            if(err) {
+        conn.query(sql, ['google:' + uname], function (err, results) {
+            if (err) {
                 return done('There is no users');
             }
             let user = results[0];
@@ -137,41 +139,41 @@ passport.use(new GoogleStrategy({
     clientID: db_config.clientID,
     clientSecret: db_config.clientSecret,
     callbackURL: "/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    let newuser = {};
-    let authId = 'google:' + profile.id;
-    let sql = 'SELECT * FROM users WHERE authId=?';
-    conn.query(sql, [authId], function(err, results) {
-        if(results.length > 0) {
-            done(null, results[0]);
-        } else {
-            newuser['authId'] = authId;
-            newuser['displayName'] = profile.displayName;
-            newuser['email'] = profile.emails[0].value;
-            let sql = 'INSERT INTO users SET ?';
-            conn.query(sql, newuser, function(err, results) {
-                if(err) {
-                    console.log(err);
-                    done('Error');
-                } else {
-                    done(null, newuser);
-                }
-            });
-        }
-    });
-  }
+},
+    function (accessToken, refreshToken, profile, done) {
+        let newuser = {};
+        let authId = 'google:' + profile.id;
+        let sql = 'SELECT * FROM users WHERE authId=?';
+        conn.query(sql, [authId], function (err, results) {
+            if (results.length > 0) {
+                done(null, results[0]);
+            } else {
+                newuser['authId'] = authId;
+                newuser['displayName'] = profile.displayName;
+                newuser['email'] = profile.emails[0].value;
+                let sql = 'INSERT INTO users SET ?';
+                conn.query(sql, newuser, function (err, results) {
+                    if (err) {
+                        console.log(err);
+                        done('Error');
+                    } else {
+                        done(null, newuser);
+                    }
+                });
+            }
+        });
+    }
 ));
 app.post(
     '/auth/login',
     passport.authenticate(
-     'local',
-     { 
-        successRedirect: '/welcome',
-        failureRedirect: '/auth/login',
-        failureFlash: false
-     }
-   )
+        'local',
+        {
+            successRedirect: '/welcome',
+            failureRedirect: '/auth/login',
+            failureFlash: false
+        }
+    )
 );
 app.get(
     '/auth/google',
@@ -183,30 +185,30 @@ app.get(
 app.get(
     '/auth/google/callback',
     passport.authenticate(
-      'google',
-      {
-        successRedirect: '/welcome',
-        failureRedirect: '/auth/login'
-      }
+        'google',
+        {
+            successRedirect: '/welcome',
+            failureRedirect: '/auth/login'
+        }
     )
 );
-app.post('/auth/register', function(req, res) {
-    hasher({password:req.body.password}, function(err, pass, salt, hash) {
+app.post('/auth/register', function (req, res) {
+    hasher({ password: req.body.password }, function (err, pass, salt, hash) {
         let user = {
-            authId:'local:'+req.body.username,
+            authId: 'local:' + req.body.username,
             username: req.body.username,
             password: hash,
             salt: salt,
             displayName: req.body.displayName
         };
         let sql = 'INSERT INTO users SET ?';
-        conn.query(sql, user, function(err, results) {
-            if(err) {
+        conn.query(sql, user, function (err, results) {
+            if (err) {
                 console.log(err);
                 res.status(500);
             } else {
-                req.login(user, function(err){
-                    req.session.save(function() {
+                req.login(user, function (err) {
+                    req.session.save(function () {
                         res.redirect('/welcome');
                     });
                 });
@@ -214,6 +216,51 @@ app.post('/auth/register', function(req, res) {
         });
     });
 });
-app.listen(7260, function() {
+
+const SONG_URL_LIST = [
+    'https://soundcloud.com/amirarief/love-will-set-you-free-amirarief-kodaline-cover-mp3'
+];
+
+//ApiConstants.js
+const API_HOSTNAME = '//api.soundcloud.com';
+const CLIENT_ID = 'a281614d7f34dc30b665dfcaa3ed7505';
+
+const resolveUrl = songUrl => {
+    return `https:${API_HOSTNAME}/resolve.json?url=${songUrl}&client_id=${CLIENT_ID}`
+}
+
+app.get('/sc/test', (req, res) => {
+    let sql = 'INSERT INTO music (like_count, hashtag_1, hashtag_2, hashtag_3, play_url, date, music_name, author) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    _requestId = () => {
+        SONG_URL_LIST.map( (url)=> {
+          return axios.get(resolveUrl(url))
+                .then(response => {
+                    let like_count = 1;
+                    let hashtag_1 = response.data.tag_list;
+                    let hashtag_2 = '';
+                    let hashtag_3 = '';
+                    let play_url = response.data.permalink_url;
+                    let date = response.data.created_at;
+                    let music_name = response.data.title;
+                    let author = response.data.user.username;
+                    console.log(author);
+                    conn.query(sql, [like_count, hashtag_1, hashtag_2, hashtag_3, play_url, date, music_name, author], function(err, rows, fields) {
+                        if(err) {
+                            console.log(err);
+                        } else {
+                            res.send(response.data);
+                        }
+                    });
+                })
+                .catch(err =>{
+                    console.log(err);  
+                })
+        })
+      }
+
+    _requestId();
+});
+
+app.listen(7260, function () {
     console.log('Connected 7260 port!!!');
 });
