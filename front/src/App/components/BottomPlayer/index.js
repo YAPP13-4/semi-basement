d302bf90d5e4c0cad201e8c93db3a707bc131942
SoYoung210 <ethdud1@gmail.com>
2018-09-23 23:06:39 +0900

    [#27] feat : toggel History용 버튼과 액션 셋팅

--- front/src/App/components/BottomPlayer/index.js
+++ front/src/App/components/BottomPlayer/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 import classnames from 'classnames/bind'
-import {connect} from 'react-redux' 
+import { connect } from 'react-redux' 
+import { bindActionCreators } from 'redux'
+import { toggleHistory } from '../../../redux/meta/actions'
 import css from './index.scss'
 import { SONG_URL } from '../../constants/ApiConstants'
 const cx = classnames.bind(css)
@@ -14,14 +16,19 @@ const moduleName = 'BottomPlayer'
           <div className={cx(`${moduleName}`)}>
 
             <ReactPlayer className={cx(`${moduleName}-player`) } url={this.props.song} controls={true}/>
+            <button onClick={this.props.toggleHistory}> history  </button>
           </div>
         )
       }
 }
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({toggleHistory} , dispatch)
+}
+
 
 function mapStateToProps(state) {
   const songurl = (SONG_URL.replace(':id', state.music.song));
   return { song : songurl}
 } 
 
-export default connect(mapStateToProps)(BottomPlayer)
+export default connect(mapStateToProps,mapDispatchToProps)(BottomPlayer)
--- front/src/redux/meta/actions.js
+++ front/src/redux/meta/actions.js
@@ -5,6 +5,7 @@ export const EXAMPLE = `${MODULE_NAME}/EXAMPLE`
 export const EXAMPLE_REQUEST = `${MODULE_NAME}/EXAMPLE_REQUEST`
 export const EXAMPLE_SUCCESS = `${MODULE_NAME}/EXAMPLE_SUCCESS`
 export const EXAMPLE_FAILURE = `${MODULE_NAME}/EXAMPLE_FAILURE`
+export const TOGGLE_HISTORY = `${MODULE_NAME}/TOGGLE_HISTORY`
 
 export function example() {
   return {
@@ -28,5 +29,12 @@ export function exampleFailure(error) {
   return {
     type: EXAMPLE_FAILURE,
     error
+  }
+}
+
+export function toggleHistory() {
+  console.log('toggle')
+  return {
+    type: TOGGLE_HISTORY
   }
 }
