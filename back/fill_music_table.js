e1f734039116a248835f514a2e08dbb7b3449ea9
boribap <bsww201@naver.com>
2018-09-24 00:56:04 +0900

    #32 해시태그 값 제대로 넣기 완료

--- back/fill_music_table.js
+++ back/fill_music_table.js
@@ -3,9 +3,9 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const axios = require('axios');
-const db_config = require('./db_config.json');
 const mysql = require('mysql');
 
+const db_config = require('./db_config.json');
 const conn = mysql.createConnection({
     host      : db_config.host,
     user      : db_config.user,
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 app.use(bodyparser.urlencoded({ extended: false }));
 
 // 1. 사운드 클라우드 음악 url에서 정보를 추출하기 
-//      --> 날짜
+//      --> 날짜, 저작자, 해시태그, 음악제목, url, id
 // 2. music 테이블에 삽입
 
 const API_HOSTNAME = '//api.soundcloud.com';
@@ -28,16 +28,15 @@ const CLIENT_ID = 'a281614d7f34dc30b665dfcaa3ed7505';
 const resolveUrl = songUrl => `https:${API_HOSTNAME}/resolve.json?url=${songUrl}&client_id=${CLIENT_ID}`
 
 const SONG_URL_LIST = [
-    // 'https://soundcloud.com/amirarief/love-will-set-you-free-amirarief-kodaline-cover-mp3',
+     'https://soundcloud.com/amirarief/love-will-set-you-free-amirarief-kodaline-cover-mp3',
     // 'https://soundcloud.com/filous/kodaline-high-hopes-filous',
     // 'https://soundcloud.com/amirarief/love-will-set-you-free-amirarief-kodaline-cover-mp3',
     // 'https://soundcloud.com/filous/kodaline-high-hopes-filous',
-     'https://soundcloud.com/in-demand-scotland/kodaline-all-i-want-session',
+    // 'https://soundcloud.com/in-demand-scotland/kodaline-all-i-want-session',
     // 'https://soundcloud.com/filous/kodaline-high-hopes-filous'
+    // 'https://soundcloud.com/youngma/young-ma-ooouuu-1'
 
 ];
-
-var rr = new Array();
 
 app.get('/fillMusicTable', function(req, res) {
 
@@ -56,13 +55,34 @@ app.get('/fillMusicTable', function(req, res) {
                   return axios.get(resolveUrl(url))
                         .then(response => {
                             let sc_id = response.data.id;
-                            let hashtag_1 = response.data.tag_list;
-                            let hashtag_2 = '';
-                            let hashtag_3 = '';
                             let play_url = response.data.permalink_url;
                             let date = response.data.created_at;
                             let music_name = response.data.title;
                             let author = response.data.user.username;
+                            var hashtag_1 = '';
+                            var hashtag_2 = '';
+                            var hashtag_3 = '';
+
+                            var rr = new Array();
+                            rr = response.data.tag_list.split('\"');
+
+                            //길이가 1이면 0개 혹은 1개 
+                            //길이가 1보다 크면 태그가 여러개 
+
+                            // rr[0]이 공백이면 태그 0개
+
+                            if(rr.length == 1){
+                                if(rr[0] != ''){
+                                    hashtag_1 = rr[0];
+                                }
+                            } else{
+                                hashtag_1 = rr[1];
+                                hashtag_2 = rr[2];
+                                hashtag_3 = rr[3];
+                            }
+
+                            res.send(response.data);
+
 
                             for (var i = 0; i < count_row.length; i++){
                                 if(count_row[i].sc_id == response.data.id){
@@ -77,7 +97,6 @@ app.get('/fillMusicTable', function(req, res) {
                                         console.log(err);
                                     }else{
                                         console.log("update done");
-                                        res.send(response.data);
                                     }
                                 })
                             }else if(updateTrigger == 0){
@@ -86,7 +105,6 @@ app.get('/fillMusicTable', function(req, res) {
                                         console.log(err);
                                     } else {
                                         console.log("insert done");
-                                        res.send(response.data);
                                     }
                                 });    
                             }
