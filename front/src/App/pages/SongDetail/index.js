72c180c0bd317e28298d395a411be373650a8a41
livelikeabel <esung1129@gmail.com>
2018-09-24 16:36:34 +0900

    #34 add song detail grid

--- front/src/App/pages/SongDetail/index.js
+++ front/src/App/pages/SongDetail/index.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'recompose'
 
 import classnames from 'classnames/bind'
 import css from './index.scss'
 
 const cx = classnames.bind(css)
-const moduleName = "SongDetail"
+const moduleName = 'SongDetail'
 
 class SongDetail extends Component {
   render() {
     return (
       <div className={cx(`${moduleName}`)}>
-        <div className={cx(`${moduleName}-music`)}>album</div>
+        <div className={cx(`${moduleName}-music`)}>
+          <img className={cx(`${moduleName}-albumCover`)} alt="albumCover" />
+          <div className={cx(`${moduleName}-wordings`)}>wordings</div>
+        </div>
         <div className={cx(`${moduleName}-infoWrapper`)}>
           <div className={cx(`${moduleName}-songInfo`)}>앨범소개</div>
           <div className={cx(`${moduleName}-coments`)}>댓글</div>
@@ -20,4 +25,11 @@ class SongDetail extends Component {
   }
 }
 
-export default SongDetail
+export default compose(
+  connect((state, props) => {
+    const { music } = state
+    return {
+      songDetail: music.songDetail
+    }
+  })
+)(SongDetail)
