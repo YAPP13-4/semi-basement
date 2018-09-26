43d9be4bc7dbcc7325419d3153782a6eb7df9f34
SoYoung210 <ethdud1@gmail.com>
2018-09-26 18:54:35 +0900

    [#37] fix : reducer import 경로 오타 ..................

--- front/src/redux/reducers.js
+++ front/src/redux/reducers.js
@@ -1,6 +1,6 @@
 import meta from './meta/reducer';
 import music from './music/reducer';
-import player from './music/reducer'
+import player from './player/reducer'
 
 // 이는 바깥에서 combine 해준다.
 export default {
