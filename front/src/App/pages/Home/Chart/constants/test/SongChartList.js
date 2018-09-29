0b9091c0db79d20aa753fb80f432d8e9d6a8b4f8
SoYoung210 <ethdud1@gmail.com>
2018-09-29 19:37:39 +0900

    [commons] ChartTab test url  변경

--- front/src/App/pages/Home/Chart/constants/test/SongChartList.js
+++ front/src/App/pages/Home/Chart/constants/test/SongChartList.js
@@ -1,8 +1,8 @@
 const SongChartList = [
     'https://soundcloud.com/amirarief/love-will-set-you-free-amirarief-kodaline-cover-mp3',
     'https://soundcloud.com/filous/kodaline-high-hopes-filous',
-    'https://soundcloud.com/amirarief/love-will-set-you-free-amirarief-kodaline-cover-mp3',
-    'https://soundcloud.com/filous/kodaline-high-hopes-filous',
+    'https://soundcloud.com/undiscoveredsounds/coldplay-ft-beyonce-hymn-for-the-weekend-ash-remix',
+    'https://soundcloud.com/housemusic/richy-fancy-low',
     'https://soundcloud.com/in-demand-scotland/kodaline-all-i-want-session',
     'https://soundcloud.com/filous/kodaline-high-hopes-filous'
 
