b2fa7de66c588e2e2cc20dd062e6adac5285252e
SoYoung210 <ethdud1@gmail.com>
2018-10-02 22:28:44 +0900

    [ ] codeStyle : prettier 적용

--- front/src/redux/music/actions.js
+++ front/src/redux/music/actions.js
@@ -1,16 +1,15 @@
-import * as types from './ActionType';
+import * as types from "./ActionType";
 export function selectSong(song) {
-    return {
-        type: types.SELECT_SONG,
-        song
-    }
+  return {
+    type: types.SELECT_SONG,
+    song
+  };
 }
 export function addHistory(historySong) {
-    return {
-        type: types.HISTORY_SONG,
-        historySong
-    }
-        
+  return {
+    type: types.HISTORY_SONG,
+    historySong
+  };
 }
 /*
 export const selectSong = song=> ({
