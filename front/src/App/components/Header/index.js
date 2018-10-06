56df6931714d59cfdf398dab4441bca471e94bbe
DESKTOP-CPF5K67\yoobin <qorrn3574@naver.com>
2018-10-06 17:59:27 +0900

    #35 로그인창

--- front/src/App/components/Header/index.js
+++ front/src/App/components/Header/index.js
@@ -3,11 +3,16 @@ import { connect } from 'react-redux'
 import { compose } from 'redux'
 import classnames from 'classnames/bind'
 import logo from '../../../assets/logos/logo.png'
+import google from '../../../assets/icons/google.png'
+import facebook from '../../../assets/icons/facebook.png'
+import twitter from '../../../assets/icons/twitter.png'
+
 import css from './index.scss'
 import Popup from "reactjs-popup";
 
 const cx = classnames.bind(css)
 const moduleName = 'Header'
+
 class Header extends React.Component {
   render() {
     return (
@@ -18,7 +23,9 @@ class Header extends React.Component {
         }}
       >
         <div className={cx(`${moduleName}-menu`)}>
-          <span></span>
+          <span>
+
+          </span>
         </div>
         <div className={cx(`${moduleName}-logo`)}>
             <img src={logo} alt="logo" />
@@ -37,16 +44,22 @@ class Header extends React.Component {
                      <a className={cx(`${moduleName}-modal-close`)} onClick={close}>
                         &times;
                       </a>
-                    <div className={cx(`${moduleName}-modal-title`)}> Welcome semibasement </div>
+                    <div className={cx(`${moduleName}-modal-title`)}> Welcome semibasement! </div>
                     <br></br><br></br><br></br>
                     <div className={cx(`${moduleName}-modal-content`)}>
-                    <button className={cx(`${moduleName}-modal-content-login`)}>Continue with KakaoTalk</button>
+                    <button className={cx(`${moduleName}-modal-content-login`)}>
+                    <img style={{marginright: 20}} src={google} alt="google" />
+                    Continue with google</button>
                     <br></br> <br></br> <br></br>
                     <hr/>
-                    <br></br>
-                    <button className={cx(`${moduleName}-modal-content-login2`)}>Continue with Facebook</button>
+                    <br></br><br></br>
+                    <button className={cx(`${moduleName}-modal-content-login2`)}>
+                    <img src={facebook} alt="facebook" />
+                    Continue with Facebook</button>
                     <br></br> <br></br>
-                    <button className={cx(`${moduleName}-modal-content-login2`)}>Continue with Twitter</button>
+                    <button className={cx(`${moduleName}-modal-content-login2`)}>
+                    <img src={twitter} alt="twitter" />
+                    Continue with Twitter</button>
                     <br></br><br></br><br></br>
                       
                     </div>
