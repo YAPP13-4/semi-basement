9886ffbb6e1dfb64638e82401912a9655c7cae9b
SoYoung210 <ethdud1@gmail.com>
2018-10-12 00:30:47 +0900

    [#73] test : test URL 셋팅

--- front/src/App/constants/test/SongUrlConstants.js
+++ front/src/App/constants/test/SongUrlConstants.js
@@ -1,11 +1,10 @@
 const SONG_URL_LIST = [
-    'https://soundcloud.com/amirarief/love-will-set-you-free-amirarief-kodaline-cover-mp3',
-    'https://soundcloud.com/filous/kodaline-high-hopes-filous',
-    'https://soundcloud.com/amirarief/love-will-set-you-free-amirarief-kodaline-cover-mp3',
-    'https://soundcloud.com/filous/kodaline-high-hopes-filous',
-    'https://soundcloud.com/in-demand-scotland/kodaline-all-i-want-session',
-    'https://soundcloud.com/filous/kodaline-high-hopes-filous'
+  "https://soundcloud.com/dailywshh/lil-baby-seattle",
+  "https://soundcloud.com/godtjd777/sets/pluto",
+  "https://soundcloud.com/lxwl4ih4tphn/0fsjwwdgv95b",
+  "https://soundcloud.com/kieran-hume/zara-larsson-mnek-never-forget-you",
+  "https://soundcloud.com/in-demand-scotland/kodaline-all-i-want-session",
+  "https://soundcloud.com/rd-urbans-music/unica-ozuna-original"
+]
 
-];
-
-export default SONG_URL_LIST;
+export default SONG_URL_LIST
---
+++ front/src/App/constants/test/SongUrlConstants2.js
@@ -0,0 +1,10 @@
+const SONG_URL_LIST1 = [
+  "https://soundcloud.com/tommisch/sets/beat-tape-1",
+  "https://soundcloud.com/seung-ryeol-paik/nuit-swing-demo-with-decalcomanic",
+  "https://soundcloud.com/lxwl4ih4tphn/feat-planet",
+  "https://soundcloud.com/namqkang/blueberrydream",
+  "https://soundcloud.com/link6/a-june-j-beat-movinon-featazitiz",
+  "https://soundcloud.com/user-61321127/swangg"
+]
+
+export default SONG_URL_LIST1
