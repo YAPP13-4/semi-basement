a93721947ff395e3bba91e7c33e2fc2d4c2c6205
livelikeabel <esung1129@gmail.com>
2018-10-19 00:45:42 +0900

    #57 repactor at ArtworkPlay what props

--- front/src/App/pages/Home/components/ArtworkPlay.js
+++ front/src/App/pages/Home/components/ArtworkPlay.js
@@ -1,16 +1,14 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import classnames from "classnames/bind"
-import getImageUrl from "src/utils/ImageUtils"
-import css from "./ArtworkPlay.scss"
-import IMAGE_SIZES from "../../../constants/ImageConstants"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import classnames from 'classnames/bind'
+import getImageUrl from 'src/utils/ImageUtils'
+import css from './ArtworkPlay.scss'
+import IMAGE_SIZES from '../../../constants/ImageConstants'
 
 const cx = classnames.bind(css)
-const moduleName = "ArtworkPlay"
+const moduleName = 'ArtworkPlay'
 
-const ArtWorkPlay = ({ music, selectSong, addHistory, loadSongDetail }) => {
-  debugger
-  const { musicInfo } = music
+const ArtWorkPlay = ({ musicInfo, onClickPlay, onClickSongDetail }) => {
   const songId = musicInfo.id
   const title = musicInfo.title
   const creator = musicInfo.user.username
@@ -24,10 +22,8 @@ const ArtWorkPlay = ({ music, selectSong, addHistory, loadSongDetail }) => {
           <div className={cx(`${moduleName}-song-body-card`)}>
             <div
               className={cx(`${moduleName}-song-body-card-artwork`)}
-              //FIX ME
               onClick={() => {
-                selectSong([songId, title, artworkUrl, duration])
-                addHistory(songId)
+                onClickPlay({ songId, title, artworkUrl, duration })
               }}
               style={{
                 backgroundImage: `url(${getImageUrl(
@@ -42,9 +38,8 @@ const ArtWorkPlay = ({ music, selectSong, addHistory, loadSongDetail }) => {
             <Link
               className={cx(`${moduleName}-song-body-card-link`)}
               to="/songDetail"
-              //FIXME
               onClick={() => {
-                loadSongDetail(songId)
+                onClickSongDetail(songId)
               }}
             >
               <div className={cx(`${moduleName}-song-body-card-info`)}>
@@ -63,69 +58,3 @@ const ArtWorkPlay = ({ music, selectSong, addHistory, loadSongDetail }) => {
   )
 }
 export default ArtWorkPlay
-/*
-class ArtworkPlay extends Component {
-  static defaultProps = {
-    singerName: "Creator",
-    title: "Title",
-    artwork: "ArtWork"
-  }
-  _fetchSong = () => {
-    //이 부분에 배열로 담아서 store에 저장하기.
-    console.log("click", this.props.songId)
-    //son id, title, artwork, duration --> song array
-    const songInfo = [
-      this.props.songId,
-      this.props.title,
-      this.props.artwork,
-      this.props.duration / 1000
-    ]
-    console.log("songinfo", songInfo)
-    this.props.selectSong(songInfo)
-    this.props.addHistory(songInfo[0])
-  }
-  getSongDetail = () => {
-    this.props.loadSongDetail(this.props.songId)
-  }
-  render() {
-    const artworkUrl = this.props.artwork
-    return (
-      <div className={cx(`${moduleName}`)}>
-        <div className={cx(`${moduleName}-row`)}>
-          <div className={cx(`${moduleName}-row__cell`)}>
-            <div className={cx(`${moduleName}-song-body-card`)}>
-              <div
-                className={cx(`${moduleName}-song-body-card-artwork`)}
-                onClick={this._fetchSong}
-                style={{
-                  backgroundImage: `url(${getImageUrl(
-                    artworkUrl,
-                    IMAGE_SIZES.XLARGE
-                  )})`
-                }}
-              >
-                <div className={cx(`${moduleName}-song-body-card-playicon`)} />
-              </div>
-
-              <Link
-                className={cx(`${moduleName}-song-body-card-link`)}
-                to="/songDetail"
-                onClick={this.getSongDetail}
-              >
-                <div className={cx(`${moduleName}-song-body-card-info`)}>
-                  <div className={cx(`${moduleName}-song-body-card-title`)}>
-                    {this.props.title}
-                  </div>
-                  <div className={cx(`${moduleName}-song-body-card-singer`)}>
-                    {this.props.singerName}
-                  </div>
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-*/
--- front/src/App/pages/Home/container/ArtWrokContainer.js
+++ front/src/App/pages/Home/container/ArtWrokContainer.js
@@ -9,28 +9,30 @@ import {
 import Loading from 'src/App/components/Loading'
 import ArtworkPlay from '../components/ArtworkPlay'
 
-// const ArtWorkPlayContainer = props => {
-//   return props.music.musicInfo ? <ArtworkPlay {...props} /> : <Loading />
-// }
-
 class ArtWorkPlayContainer extends Component {
+  onClickPlay = ({ songId, title, artworkUrl, duration }) => {
+    this.props.selectSong([songId, title, artworkUrl, duration])
+    this.props.addHistory(songId)
+  }
+
+  onClickSongDetail = songId => {
+    this.props.loadSongDetail(songId)
+  }
 
   renderArtworks = () => {
-    console.log(this.props.musicInfos)
-    return this.props.musicInfos.map((musicInfo) => {
-      console.log(musicInfo)
-      debugger
-      return <ArtworkPlay { ...musicInfo} />
-      // return <ArtworkPlay songId={musicInfo.id}  />
+    return this.props.musicInfos.map(musicInfo => {
+      return (
+        <ArtworkPlay
+          musicInfo={musicInfo}
+          onClickPlay={this.onClickPlay}
+          onClickSongDetail={this.onClickSongDetail}
+        />
+      )
     })
   }
 
   render() {
-    return this.props.musicInfos ? (
-    this.renderArtworks()
-    ) : (
-      <Loading />
-    )
+    return this.props.musicInfos ? this.renderArtworks() : <Loading />
   }
 }
 
