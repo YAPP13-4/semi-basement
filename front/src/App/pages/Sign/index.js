fee26c8b0194ef885df0db7464e57fba3180dd2b
SoYoung210 <ethdud1@gmail.com>
2018-10-25 12:30:53 +0900

    [#90] style : signup Page 기본 mark up, css

--- front/src/App/pages/Sign/index.js
+++ front/src/App/pages/Sign/index.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react'
-
-import classnames from 'classnames/bind'
-import css from './index.scss'
+import React, { PureComponent } from "react"
+import logo from "src/assets/logos/logo.png"
+import classnames from "classnames/bind"
+import css from "./index.scss"
 
 const cx = classnames.bind(css)
-const moduleName = 'Sign'
+const moduleName = "SignUp"
 
-class Sign extends Component {
+class SignUp extends PureComponent {
   render() {
-    return <div className={cx(`${moduleName}`)}>Sign이야</div>
+    return (
+      <div className={cx(`${moduleName}`)}>
+        <div className={cx(`${moduleName}_wrapper`)}>
+          <div className={cx(`${moduleName}_wrapper_logo`)}>
+            <img src={logo} alt={logo} />
+          </div>
+          <div className={cx(`${moduleName}_wrapper_content`)}>
+            <div className={cx(`${moduleName}_wrapper_content_top`)}>
+              Starting Semibasement
+            </div>
+            <div className={cx(`${moduleName}_wrapper_content_body`)}>
+              복잡한 절차 없이 구글 계정으로간편하게 <br /> 세미베이스먼트를
+              만나보세요!
+            </div>
+            <div className={cx(`${moduleName}_wrapper_content_bottom`)}>
+              Continue With Google
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
-} 
+}
 
-export default Sign;
+export default SignUp
