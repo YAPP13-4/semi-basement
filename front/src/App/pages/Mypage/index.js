9d97c798116ff54d5149440109979b721c182f59
SoYoung210 <ethdud1@gmail.com>
2018-10-26 02:23:41 +0900

    [#95] feat : mypage 기본 셋팅 (nav 분리할 예정 )

--- front/src/App/pages/Mypage/index.js
+++ front/src/App/pages/Mypage/index.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
-import classnames from 'classnames/bind'
-import css from './index.scss'
-
+import classnames from "classnames/bind"
+import css from "./index.scss"
+import ChartTab from "../Home/Chart/ChartTab"
 const cx = classnames.bind(css)
-const moduleName = 'Mypage'
+const moduleName = "Mypage"
 
 class Mypage extends Component {
   render() {
-    return <div className={cx(`${moduleName}`)}>Mypage이야</div>
+    return (
+      <div className={cx(`${moduleName}`)}>
+        <div className={cx(`${moduleName}-myinfo`)}>
+          <div className={cx(`${moduleName}-myinfo-wrapper`)}>
+            <div className={cx(`${moduleName}-myinfo-wrapper-img`)}>image</div>
+            <div className={cx(`${moduleName}-myinfo-wrapper-name`)}>
+              <span>Name</span>
+            </div>
+          </div>
+        </div>
+        <div className={cx(`${moduleName}-chart`)}>
+          <div className={cx(`${moduleName}-chart-nav`)}>
+            <div>Discover</div>
+            <div>Playlist</div>
+            <div>liked</div>
+          </div>
+          <ChartTab />
+        </div>
+      </div>
+    )
   }
-} 
+}
 
-export default Mypage;
+export default Mypage
