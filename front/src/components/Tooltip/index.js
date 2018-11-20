7373f2ada6bead322333f623b56b86c867eaef14
livelikeabel <esung1129@gmail.com>
2018-11-21 00:51:04 +0900

    [#115] 기존의 툴팁 마크업과 css적용(리팩토링 필수)

--- front/src/components/Tooltip/index.js
+++ front/src/components/Tooltip/index.js
@@ -1,10 +1,17 @@
 // 상태 or 무상태...?
 // 리덕스 연결..?
 // TODO:
-// 1. 툴팁 박스
-// 2. 툴팁의 아이템들(icon, text, action(with args)) // props로 받을까...?
+// 1. 툴팁 박스 (아이템의 개수에 따라 가변적으로 변하게 하기)
+// 2. 툴팁의 아이템들(icon, text, action(with args)) // props로 받을까...? 아니면 함수의 인자로...?
 // 3. 툴팁의 event.(툴팁을 켰다가 다른곳을 누르면 꺼지게 하기)
+// 위치 바꾸어야 하나..? hoc 폴더로..?
 import React from 'react'
+
+import classnames from 'classnames/bind'
+import css from './index.scss'
+
+const cx = classnames.bind(css)
+const moduleName = 'Tooltip'
 
 const Tooltip = Component => {
   class _Tooltip extends React.Component {
@@ -15,19 +22,53 @@ const Tooltip = Component => {
     }
 
     handleClick(e) {
-      alert('click the tooltip!')
+      this.setState({ opacity: !this.state.opacity })
       e.stopPropagation()
     }
 
     render() {
-      console.log(this.state)
+      const style = {
+        opacity: +this.state.opacity,
+        zIndex: this.state.opacity ? 1000 : -1000,
+        top: 60,
+        left: -210,
+        position: 'absolute'
+      }
 
       return (
         <div>
-          <Component
-            {...this.state}
-            {...this.props}
-          />
+          <Component {...this.state} {...this.props} />
+          <div style={style} className={cx(`${moduleName}`)}>
+            <div className={cx(`${moduleName}-arrow`)} />
+            <div
+              className={cx(`${moduleName}-innerFirst`)}
+              onClick={e => {
+                this.props.onClickAdd(e)
+                this.toggle(e)
+              }}
+            >
+              <div className={cx(`${moduleName}-innerFirst-addIcon`)} />
+              <span>add</span>
+            </div>
+            <div
+              className={cx(`${moduleName}-innerSecond`)}
+              onClick={() => {
+                alert('share!')
+              }}
+            >
+              <div className={cx(`${moduleName}-innerSecond-shareIcon`)} />
+              <span>share</span>
+            </div>
+            <div
+              className={cx(`${moduleName}-innerThird`)}
+              onClick={() => {
+                alert('like!')
+              }}
+            >
+              <div className={cx(`${moduleName}-innerThird-likeIcon`)} />
+              <span>like</span>
+            </div>
+          </div>
         </div>
       )
     }
