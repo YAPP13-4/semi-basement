417fabca96402a7d7459d6a6819afee069b70e0d
SoYoung210 <ethdud1@gmail.com>
2018-12-03 15:22:23 +0900

    [#129] 🎁feat : music reducer 중 loadSongInfo , selectSong TestCode작성

---
+++ front/src/test/reducers/music.reducers.test.js
@@ -0,0 +1,63 @@
+import musicReducer from 'src/redux/music/reducer.js';
+import * as musicActions from 'src/redux/music/actions.js';
+import { musicInfoMockData } from '../mockData';
+
+describe('Test music Reducer', () => {
+  function getInitialState() {
+    return {
+      song: '',
+      loading: false,
+      infoLoading: false,
+      musicInfo: null,
+      recommendMusicInfo1: null,
+      recommendMusicInfo2: null,
+      songDetail: null,
+      historySong: [],
+      searchKeyword: '',
+      searchResult: null,
+    };
+  }
+  it('Test loadSongInfoSuccess ', () => {
+    //Given
+    const mockData = musicInfoMockData;
+    const loadSongSuccessAction = {
+      type: musicActions.LOAD_SONG_INFO_SUCCESS,
+      data: [mockData],
+    };
+    const initialState = getInitialState();
+
+    //When
+    const result = musicReducer(initialState, loadSongSuccessAction);
+
+    //Then
+    const expected = {
+      ...initialState,
+      infoLoading: false,
+      musicInfo: [mockData],
+    };
+    expect(result).toEqual(expected);
+  });
+  //뭐랄까 createAction.test.js랑 상당히 비슷한 느낌. ;
+  it('Test Select Song', () => {
+    //Given
+    const mockSong = [
+      331622174,
+      '지금 뭐 해 (feat. PLANET)',
+      'https://i1.sndcdn.com/artworks-000232037629-yspo52-large.jpg',
+      186.479,
+    ];
+    const selectSongAction = {
+      type: musicActions.SELECT_SONG,
+      song: mockSong,
+    };
+    const initialState = getInitialState();
+    //When
+    const result = musicReducer(initialState, selectSongAction);
+    //Then
+    const expected = {
+      ...initialState,
+      song: mockSong,
+    };
+    expect(result).toEqual(expected);
+  });
+});
