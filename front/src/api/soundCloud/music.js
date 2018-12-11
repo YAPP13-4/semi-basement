ce9975e064752646b3d86631b7eaf805bd723302
eunjeongsong <hellogine12@gmail.com>
2018-12-12 01:02:58 +0900

    에러 핸들링

--- front/src/api/soundCloud/music.js
+++ front/src/api/soundCloud/music.js
@@ -1,19 +1,21 @@
-import { SONG_URL, resolveUrl } from "../apiConstants"
-import axios from "axios"
+import { SONG_URL, resolveUrl } from '../apiConstants';
+import axios from 'axios';
 
 export function getSoundCloudSongInfo(url) {
   //debugger
   return axios
     .get(resolveUrl(url))
     .then(response => response.data)
-    .catch(err => err)
+    .catch(err => {
+      console.log(err);
+      return Promise.reject(err);
+    });
 }
 
 export function getSoundCloudSong(songId) {
-  const songUrl = SONG_URL.replace(":id", songId)
-
+  const songUrl = SONG_URL.replace(':id', songId);
   return axios
     .get(songUrl)
     .then(res => res)
-    .then(err => err)
+    .then(err => err);
 }
