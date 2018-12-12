8e700d21f8980a0ef24a64b016422d83f5158ce1
eunjeongsong <hellogine12@gmail.com>
2018-12-13 00:24:39 +0900

    no message

--- front/src/api/soundCloud/music.js
+++ front/src/api/soundCloud/music.js
@@ -2,20 +2,10 @@ import { SONG_URL, resolveUrl } from '../apiConstants';
 import axios from 'axios';
 
 export function getSoundCloudSongInfo(url) {
-  //debugger
-  return axios
-    .get(resolveUrl(url))
-    .then(response => response.data)
-    .catch(err => {
-      console.log(err);
-      return Promise.reject(err);
-    });
+  return axios.get(resolveUrl(url)).then(response => response.data);
 }
 
 export function getSoundCloudSong(songId) {
   const songUrl = SONG_URL.replace(':id', songId);
-  return axios
-    .get(songUrl)
-    .then(res => res)
-    .then(err => err);
+  return axios.get(songUrl);
 }
