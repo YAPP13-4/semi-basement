f21e3b5ef0670479d459961600777279ac321251
abelko <esung1129@gmail.com>
2019-01-05 18:14:10 +0900

    [#165] refactor at Home component - remove dummy data - remove redux at Home component

--- src/App/pages/Home/index.js
+++ src/App/pages/Home/index.js
@@ -1,18 +1,22 @@
+/**
+ * module imports
+ */
 import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-
-import Navigation from './components/Navigation';
-import Loading from 'src/App/components/Loading';
-import Featured from './components/Featured';
-import { loadMusicsInfo } from 'src/redux/music/actions';
-//TODO : FIX (with BE)
-import MUSIC_URL_LIST1 from '../../constants/test/MusicUrlConstants1';
-import ArtWorkPlayContainer from '../Home/container/ArtWorkPlayContainer';
 import classnames from 'classnames/bind';
-import css from './index.scss';
-
-// 임시
 import axios from 'axios';
+
+/**
+ * src path
+ */
+import Loading from 'src/App/components/Loading';
+
+/**
+ * relative path
+ */
+import Navigation from './components/Navigation';
+import Featured from './components/Featured';
+import ArtWorkPlayContainer from '../Home/container/ArtWorkPlayContainer';
+import css from './index.scss';
 
 const cx = classnames.bind(css);
 const moduleName = 'Home';
@@ -25,17 +29,27 @@ class Home extends PureComponent {
   };
 
   componentDidMount() {
-    this.props.loadMusicsInfo(MUSIC_URL_LIST1);
     axios.get('http://localhost:6508/musics/seba-choice').then(res => {
       this.setState({ sebaChoice: res.data });
     });
   }
 
   render() {
-    // return !this.props.mainMusicLoading && !this.props.subMusicLoading ? (
     return this.state.sebaChoice.length ? (
       <div className={cx(`${moduleName}`)}>
         <Navigation />
+        <div>
+          <ArtWorkPlayContainer
+            category="Seba's Choice"
+            musicInfos={this.state.sebaChoice}
+          />
+        </div>
+        <div>
+          <ArtWorkPlayContainer
+            category="Seba's Choice"
+            musicInfos={this.state.sebaChoice}
+          />
+        </div>
         <div>
           <ArtWorkPlayContainer
             category="Seba's Choice"
@@ -52,14 +66,4 @@ class Home extends PureComponent {
   }
 }
 
-export default connect(
-  ({ music }) => {
-    return {
-      musicInfos: music.musicInfo,
-      mainMusicLoading: music.loading,
-    };
-  },
-  {
-    loadMusicsInfo,
-  },
-)(Home);
+export default Home;
