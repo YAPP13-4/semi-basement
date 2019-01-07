15b50f2fe4abec32d2c26078ba2f376fe6d4ea93
abelko <esung1129@gmail.com>
2019-01-08 08:27:00 +0900

    [#165] saga 수정 (1/2)

--- src/redux/player/sagas.js
+++ src/redux/player/sagas.js
@@ -7,21 +7,31 @@ export function* playNextMusic(action) {
   yield put(palyerActions.playNextMusicRequest());
 
   const isShuffle = yield select(state => state.player.shuffle);
-  const currentMusicInfo = yield select(state => state.music.playingMusic);
+  const currentMusic = yield select(state => state.music.playingMusic);
   const targetPlayList = yield select(state => state.playList.musicList);
-  try {
-    if (targetPlayList) {
-      const currentMusicIndex = targetPlayList.indexOf(
-        currentMusicInfo.id,
-      );
-      const nextMusicId = isShuffle
-        ? targetPlayList[randomIndex(currentMusicIndex, targetPlayList.length)]
-        : targetPlayList[(currentMusicIndex + 1) % targetPlayList.length];
 
-      const data = yield call(getSoundCloudMusic, nextMusicId);
-      console.log('nexmusic test',currentMusicIndex,nextMusicId,data)
-      yield put(palyerActions.playNextMusicSuccess(data));
-    }
+  try {
+    // 나중에 shuffle 기능 리팩토링때 참고하기 위해 남겨놓음
+    // if (targetPlayList) {
+    //   const currentMusicIndex = targetPlayList.indexOf(currentMusicInfo.id);
+    //   const nextMusicId = isShuffle
+    //     ? targetPlayList[randomIndex(currentMusicIndex, targetPlayList.length)]
+    //     : targetPlayList[(currentMusicIndex + 1) % targetPlayList.length];
+
+    //   const data = yield call(getSoundCloudMusic, nextMusicId);
+    //   yield put(palyerActions.playNextMusicSuccess(data));
+    // }
+
+    // 현재 재생중인 곡의 id를 통해, 현재 재생목록에서의 위치를 찾고, 그 다음곡을 select 해준다.
+    console.log(currentMusic.id);
+    console.log(targetPlayList);
+    console.log(
+      targetPlayList.filter((music, index) => {
+        console.log(index);
+        return music.id === currentMusic.id;
+      }),
+    );
+    debugger;
   } catch (err) {
     yield put(palyerActions.playNextMusicFailure(err));
   }
@@ -36,9 +46,7 @@ export function* playPrevMusic(action) {
 
   try {
     if (targetPlayList) {
-      const currentMusicIndex = targetPlayList.indexOf(
-        currentMusicInfo.id,
-      );
+      const currentMusicIndex = targetPlayList.indexOf(currentMusicInfo.id);
       let nextMusicId = targetPlayList[0];
       if (currentMusicIndex !== -1)
         nextMusicId = isShuffle
