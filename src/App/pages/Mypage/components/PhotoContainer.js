c7c8836dcfb60cbfa1b20e34697cfd024d3b2e75
SoYoung210 <ethdud1@gmail.com>
2019-01-31 20:29:13 +0900

    Add unsplash photo component
    - #172

--- src/App/pages/Mypage/components/PhotoContainer.js
+++ src/App/pages/Mypage/components/PhotoContainer.js
@@ -1,45 +1,71 @@
-import React, { Component } from "react"
-import axios from 'axios'
-import classnames from "classnames/bind"
-import { PhotoSearchForm } from './PhotoForm'
-import { USP_CLIENT_ID } from 'src/App/constants/ApiConstants'
-import css from "./PhotoContainer.scss"
+import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames/bind';
+import { PhotoSearchForm } from './PhotoForm';
+import { PhotoComponent } from './PhotoComponent';
+import { USP_CLIENT_ID } from 'src/App/constants/ApiConstants';
+import css from './PhotoContainer.scss';
 
-const cx = classnames.bind(css)
-const moduleName = "PhotoContainer"
+const cx = classnames.bind(css);
+const moduleName = 'PhotoContainer';
 
 export class PhotoContainer extends Component {
-    state = {
-        photoList: null,
-        keyword: ''
-    }
-    componentDidMount() {
-        this.fetchUnsplashPhoto()
-    }
-    render() {
-        return(
-            <div className={cx(`${moduleName}`)}>
-                Photos by Unsplash
-                <PhotoSearchForm onChange={this.changeSearchKeyword} />
-            </div>
-        )
-    }
+  state = {
+    photoList: null,
+    keyword: '',
+    fetched: false,
+  };
+  componentDidMount() {
+    this.fetchUnsplashPhoto();
+  }
+  render() {
+    return (
+      <div className={cx(`${moduleName}`)}>
+        Photos by Unsplash
+        <PhotoSearchForm onChange={this.changeSearchKeyword} />
+        <div className={cx(`${moduleName}-photowrapper`)} >
+        { this.renderPhotoComponent() }
+        </div>
+      </div>
+    );
+  }
 
-    changeSearchKeyword = (keyword) => {
+  renderPhotoComponent = () => {
+      const { photoList, fetched } = this.state
+
+      if(!fetched) return;
+      return photoList.results.map((photo,index) => (
+        <PhotoComponent key={index} photo={photo} />
+      ))
+  }
+
+  changeSearchKeyword = keyword => {
+    this.setState(
+      {
+        keyword,
+      },
+      () => {
+        this.fetchUnsplashPhoto();
+      },
+    );
+  };
+
+  fetchUnsplashPhoto = () => {
+    const { keyword } = this.state;
+    if(!keyword) return
+    axios
+      .get(
+        `https://api.unsplash.com/search/photos?page=1&query=${keyword}&client_id=${USP_CLIENT_ID}`,
+      )
+      .then(res => {
         this.setState({
-            keyword 
-        },
-            ()=>{this.fetchUnsplashPhoto()}
-        )
-    }
-
-    fetchUnsplashPhoto = () => {
-        const { keyword } = this.state
-        axios.get(`https://api.unsplash.com/search/photos?page=1&query=${keyword}&client_id=${USP_CLIENT_ID}`)
-        .then(res => {
+            photoList: res.data,
+        }, () => {
+            console.log('this state ', this.state.photoList)
             this.setState({
-                photoList: res.data
+                fetched: true
             })
-        })
-    }
+        });
+      });
+  };
 }
