5f7bb43d7377cf8a1e9530e4b29fceb7652b2865
SoYoung210 <ethdud1@gmail.com>
2019-02-04 19:06:56 +0900

    Add PhotoForm styling
    #172

--- src/App/pages/Mypage/components/PhotoForm.js
+++ src/App/pages/Mypage/components/PhotoForm.js
@@ -1,9 +1,16 @@
 
 import React from "react"
+import classnames from 'classnames/bind';
+import css from './PhotoForm.scss';
+
+const cx = classnames.bind(css);
+const moduleName = 'PhotoSearchForm';
 
 export const PhotoSearchForm = (props) => {
     const { onChange } = props; 
     return (
-        <input onChange={e => onChange(e.target.value)}></input>
+        <div className={cx(`${moduleName}`)}>
+            <input placeholder="Search Photos"  onChange={e => onChange(e.target.value)}></input>
+        </div>
     )
 }
