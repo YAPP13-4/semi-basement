374962c82eeba332aaafa551690ae36c91bff033
abelko <esung1129@gmail.com>
2019-02-05 10:07:44 +0900

    [#173]:sparkles: form 마크업 및 handler추가

--- src/App/pages/MusicRegister/index.js
+++ src/App/pages/MusicRegister/index.js
@@ -5,15 +5,53 @@ import css from './index.scss';
 const cx = classnames.bind(css);
 const moduleName = 'MusicRegister';
 class MusicRegister extends Component {
+    
+    state = {
+        url:'',
+        title: '',
+        musician: '',
+        lyrics: '',
+        description: '',
+        albumCover: ''
+    }
+    
+    handleChange = (e) => {
+        const {name, value} = e.target
+        this.setState({[name]: value})
+    }
+    
+    
     render() {
         return <div className={cx(`${moduleName}`)}>
             <h1>Register Song</h1>
             <div className={cx(`${moduleName}-body`)}>
                 <div className={cx(`${moduleName}-inputs`)}>
-                    inputs
+                    <form>
+                        <label>
+                            URL
+                            <input name='url' value={this.state.url} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Title
+                            <input name='title' value={this.state.title} onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Musician
+                            <input name='musician' value={this.state.musician} onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Lyrics
+                            <textarea name='lyrics' value={this.state.lyrics} onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Description
+                            <textarea name='description' value={this.state.description} onChange={this.handleChange}/>
+                        </label>
+                        <input name="albumCover" value={this.state.albumCover}/>
+                    </form>
                 </div>
                 <div className={cx(`${moduleName}-albumCover`)}>
-                    ablum cover
+                    album cover
                 </div>
             </div>
         </div>
