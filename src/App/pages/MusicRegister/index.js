4a8c4ef3c0ef021f9556f10212c11a3e234a4e8f
abelko <esung1129@gmail.com>
2019-02-16 18:11:45 +0900

    [#173]:sparkles: register actions 추가

--- src/App/pages/MusicRegister/index.js
+++ src/App/pages/MusicRegister/index.js
@@ -13,158 +13,159 @@ const cx = classnames.bind(css);
 const moduleName = 'MusicRegister';
 class MusicRegister extends Component {
 
-    state = {
-        url: '',
-        title: '',
-        musician: '',
-        lyrics: '',
-        description: '',
-        artworkImg: '',
-        selectedArtworkImg: '',
-        isAgree: false
-    }
+  state = {
+    url: '',
+    title: '',
+    musician: '',
+    lyrics: '',
+    description: '',
+    artworkImg: '',
+    selectedArtworkImg: '',
+    isAgree: false
+  }
 
-    handleChange = e => {
-        const target = e.target
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const id = target.id
+  handleChange = e => {
+    const target = e.target
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const id = target.id
 
-        this.setState({[id]: value})
-    }
+    this.setState({[id]: value})
+  }
 
-    handleKeyDown = e => {
-        if (e.key === 'Enter') e.preventDefault()
-    }
+  handleKeyDown = e => {
+    if (e.key === 'Enter') e.preventDefault()
+  }
 
-    handleSubmit = e => {
-        e.preventDefault()
-        if (!this.state.isAgree) return alert('plz check the box');
-        const {url, title, musician, lyrics, description, artworkImg} = this.state
-        postMusic({url, title, musician, lyrics, description, artworkImg})
-        console.log('서브밋!')
-    }
+  handleSubmit = e => {
+    e.preventDefault()
+    if (!this.state.isAgree) return alert('plz check the box');
+    const {url, title, musician, lyrics, description, artworkImg} = this.state
+    postMusic({url, title, musician, lyrics, description, artworkImg})
+    console.log('서브밋!')
+  }
 
-    fetchMusicInfo = url => e => {
-        getMusicInfo(url).then(({title, musician, description, artworkImg}) => {
-            this.setState({title, musician, description, artworkImg, selectedArtworkImg: artworkImg})
-        })
-        e.preventDefault();
-    }
+  fetchMusicInfo = url => e => {
+    if(!url) return
+    getMusicInfo(url).then(({title, musician, description, artworkImg}) => {
+      this.setState({title, musician, description, artworkImg, selectedArtworkImg: artworkImg})
+    })
+    e.preventDefault();
+  }
 
-    renderMainArtWorkImg = () => {
-        return this.state.selectedArtworkImg
-            ? <img className={cx(`${moduleName}-albumCover-mainImg`)} src={this.state.selectedArtworkImg} alt='artworkImg' />
-            : <div className={cx(`${moduleName}-albumCover-mainImg`)}>
-                <span className={cx(`${moduleName}-albumCover-defaultLogo`)} />
-                <div className={cx(`${moduleName}-albumCover-explain`)}>
-                    <p className={cx(`${moduleName}-albumCover-explain-bold`)}>Bring album cover from SoundCloud.</p>
-                    <p className={cx(`${moduleName}-albumCover-explain-text`)}>
-                        *if you having trouble whit using artwork.<br />
-                        feel free to use our open license artwork<br />
-                        there is no responsibility and copyright
+  renderMainArtWorkImg = () => {
+    return this.state.selectedArtworkImg
+      ? <img className={cx(`${moduleName}-albumCover-mainImg`)} src={this.state.selectedArtworkImg} alt='artworkImg' />
+      : <div className={cx(`${moduleName}-albumCover-mainImg`)}>
+        <span className={cx(`${moduleName}-albumCover-defaultLogo`)} />
+        <div className={cx(`${moduleName}-albumCover-explain`)}>
+          <p className={cx(`${moduleName}-albumCover-explain-bold`)}>Bring album cover from SoundCloud.</p>
+          <p className={cx(`${moduleName}-albumCover-explain-text`)}>
+            * if you having trouble whit using artwork.<br />
+            feel free to use our open license artwork<br />
+            there is no responsibility and copyright
                     </p>
-                </div>
-            </div>
-    }
-
-    renderArtWorkImgs = imgs => {
-        return imgs.map((img, i) => (
-            <div>
-                {img
-                    ? <img
-                        onClick={() => {this.setState({selectedArtworkImg: img})}}
-                        src={img}
-                        alt="artwork"
-                        key={i} />
-                    : <div
-                        className={cx(`${moduleName}-albumCover-imgs-soundCloudLogo`)}
-                        onClick={() => {this.setState({selectedArtworkImg: ""})}}
-                    >
-                        <i />
-                    </div>}
-            </div>
-        ))
-    }
-
-    render() {
-        return <div className={cx(`${moduleName}`)}>
-            <h1>Register Song</h1>
-            <div className={cx(`${moduleName}-body`)}>
-                <form
-                    className={cx(`${moduleName}-form`)}
-                    onSubmit={this.handleSubmit} id="registerForm"
-                >
-                    <label htmlFor="url">URL</label>
-                    <input
-                        id='url'
-                        value={this.state.url}
-                        placeholder='https://'
-                        onChange={this.handleChange}
-                        onBlur={this.fetchMusicInfo(this.state.url)}
-                        onKeyDown={this.handleKeyDown}
-                    />
-
-                    <label htmlFor="title">Title</label>
-                    <input
-                        id='title'
-                        value={this.state.title}
-                        placeholder='this is title'
-                        onChange={this.handleChange}
-                        onKeyDown={this.handleKeyDown}
-                    />
-
-                    <label htmlFor="musician">Musician</label>
-                    <input
-                        id='musician'
-                        value={this.state.musician}
-                        placeholder='xxxtentacion'
-                        onChange={this.handleChange}
-                        onKeyDown={this.handleKeyDown}
-                    />
-
-                    <label htmlFor="lyrics">Lyrics</label>
-                    <textarea
-                        id='lyrics'
-                        value={this.state.lyrics}
-                        placeholder='here comes text'
-                        onChange={this.handleChange}
-                    />
-
-                    <label htmlFor="description">Description</label>
-                    <textarea
-                        id='description'
-                        value={this.state.description}
-                        placeholder='here comes text'
-                        onChange={this.handleChange}
-                    />
-
-                    <input id="artworkImg" value={this.state.selectedArtworkImg} />
-                </form>
-                <div className={cx(`${moduleName}-albumCover`)}>
-                    <h5>Album cover</h5>
-                    {this.renderMainArtWorkImg()}
-                    <div className={cx(`${moduleName}-albumCover-imgs`)}>
-                        {this.renderArtWorkImgs([this.state.artworkImg, test1, test2, test3, test4, test5])}
-                    </div>
-
-                    <div className={cx(`${moduleName}-albumCover-checkBox`)}>
-                        <input
-                            id="isAgree"
-                            type="checkbox"
-                            checked={this.state.isAgree}
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor="isAgree">I agree on the responsibility of using this artwork</label>
-                    </div>
-
-                    <div className={cx(`${moduleName}-albumCover-buttons`)}>
-                        <button>Cancel</button>
-                        <button form="registerForm">Register</button>
-                    </div>
-                </div>
-            </div>
         </div>
-    }
+      </div>
+  }
+
+  renderArtWorkImgs = imgs => {
+    return imgs.map((img, i) => (
+      <div key={i}>
+        {img
+          ? <img
+            onClick={() => {this.setState({selectedArtworkImg: img})}}
+            src={img}
+            alt="artwork"
+          />
+          : <div
+            className={cx(`${moduleName}-albumCover-imgs-soundCloudLogo`)}
+            onClick={() => {this.setState({selectedArtworkImg: ""})}}
+          >
+            <i />
+          </div>}
+      </div>
+    ))
+  }
+
+  render() {
+    return <div className={cx(`${moduleName}`)}>
+      <h1>Register Song</h1>
+      <div className={cx(`${moduleName}-body`)}>
+        <form
+          className={cx(`${moduleName}-form`)}
+          onSubmit={this.handleSubmit} id="registerForm"
+        >
+          <label htmlFor="url">URL</label>
+          <input
+            id='url'
+            value={this.state.url}
+            placeholder='https://'
+            onChange={this.handleChange}
+            onBlur={this.fetchMusicInfo(this.state.url)}
+            onKeyDown={this.handleKeyDown}
+          />
+
+          <label htmlFor="title">Title</label>
+          <input
+            id='title'
+            value={this.state.title}
+            placeholder='this is title'
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+          />
+
+          <label htmlFor="musician">Musician</label>
+          <input
+            id='musician'
+            value={this.state.musician}
+            placeholder='xxxtentacion'
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+          />
+
+          <label htmlFor="lyrics">Lyrics</label>
+          <textarea
+            id='lyrics'
+            value={this.state.lyrics}
+            placeholder='here comes text'
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="description">Description</label>
+          <textarea
+            id='description'
+            value={this.state.description}
+            placeholder='here comes text'
+            onChange={this.handleChange}
+          />
+
+          <input id="artworkImg" value={this.state.selectedArtworkImg} />
+        </form>
+        <div className={cx(`${moduleName}-albumCover`)}>
+          <h5>Album cover</h5>
+          {this.renderMainArtWorkImg()}
+          <div className={cx(`${moduleName}-albumCover-imgs`)}>
+            {this.renderArtWorkImgs([this.state.artworkImg, test1, test2, test3, test4, test5])}
+          </div>
+
+          <div className={cx(`${moduleName}-albumCover-checkBox`)}>
+            <input
+              id="isAgree"
+              type="checkbox"
+              checked={this.state.isAgree}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="isAgree">I agree on the responsibility of using this artwork</label>
+          </div>
+
+          <div className={cx(`${moduleName}-albumCover-buttons`)}>
+            <button>Cancel</button>
+            <button form="registerForm">Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  }
 }
 
 export default MusicRegister
---
+++ src/redux/registerSong/actions.js
@@ -0,0 +1,18 @@
+const PREFIX = 'REGISTER_SONG';
+
+export const LOAD_SOUNDCLOUD_MUSIC_INFO = `${PREFIX}/LOAD_SOUNDCLOUD_MUSIC_INFO`;
+export const LOAD_SOUNDCLOUD_MUSIC_INFO_SUCCESS = `${PREFIX}/LOAD_SOUNDCLOUD_MUSIC_INFO_SUCCESS`;
+export const LOAD_SOUNDCLOUD_MUSIC_INFO_FAILURE = `${PREFIX}/LOAD_SOUNDCLOUD_MUSIC_INFO_FAILURE`;
+
+export const loadSoundcloudMusicInfo = ({url}) => ({
+  type: LOAD_SOUNDCLOUD_MUSIC_INFO,
+  url
+})
+
+export const loadSoundcloudMusicInfoSuccess = () => ({
+  type: LOAD_SOUNDCLOUD_MUSIC_INFO_SUCCESS
+})
+
+export const loadSoundcloudMusicInfoFailure = () => ({
+  type: LOAD_SOUNDCLOUD_MUSIC_INFO_FAILURE
+})
