c378b0afd7e86833163e165dbf05de5583731ab4
SoYoung210 <ethdud1@gmail.com>
2019-02-22 10:58:27 +0900

    Fix typo

--- src/App/pages/Mypage/components/EditForm.js
+++ src/App/pages/Mypage/components/EditForm.js
@@ -1,25 +1,33 @@
-import React, { PureComponent } from "react"
-import { SocialEditForm } from './SocialForm'
-import { PhotoContainer } from './PhotoContainer'
-import classnames from "classnames/bind"
-import css from "./EditForm.scss"
+import React, { PureComponent } from 'react';
+import { SocialEditForm } from './SocialForm';
+import { PhotoContainer } from './PhotoContainer';
+import classnames from 'classnames/bind';
+import css from './EditForm.scss';
 
-const cx = classnames.bind(css)
-const moduleName = "EditForm"
+const cx = classnames.bind(css);
+const moduleName = 'EditForm';
 
 export class EditForm extends PureComponent {
-    
-    render() {
-        const { isVisible, toggleModal } = this.props;
-        return (
-            <div className={ (isVisible) ? cx(`${moduleName}`) : cx(`${moduleName}-none`)}>
-                <h1 className={cx(`${moduleName}-title`)}>social Media</h1>
-                <span onClick={toggleModal}>Close</span>
-                <SocialEditForm title="Instargram" placeholder="input your insta address"/>
-                <SocialEditForm title="facebook" placeholder="input your facebook address"/>
-                <button> Save </button>
-                <PhotoContainer />
-            </div>
-        )
-    }
+  render() {
+    const { isVisible, toggleModal } = this.props;
+    return (
+      <div
+        className={isVisible ? cx(`${moduleName}`) : cx(`${moduleName}-none`)}>
+        <span className={cx(`${moduleName}-title`)}>social Media</span>
+        <span className={cx(`${moduleName}-close`)} onClick={toggleModal}>
+          X
+        </span>
+        <SocialEditForm
+          title="Instargram"
+          placeholder="input your insta address"
+        />
+        <SocialEditForm
+          title="facebook"
+          placeholder="input your facebook address"
+        />
+        <button> Save </button>
+        <PhotoContainer />
+      </div>
+    );
+  }
 }
